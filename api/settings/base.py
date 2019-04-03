d5674e27b2efa11d6cd55f6c7a6d2164b27099a9
Justin Nebo <nebojustin@gmail.com>
2019-04-03 12:19:20 +0100

    add booking migration and add departure and destination to flights

--- api/settings/base.py
+++ api/settings/base.py
@@ -172,10 +172,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = 8050
-# DEFAULT_FROM_EMAIL = 'admin@airtech.com'
 
 # Celery configuration
 CELERY_BROKER_URL = 'amqp://localhost'
