b968124d85623f7a9eff88cf54ccc20e5c96a9c5
Justin Nebo <nebojustin@gmail.com>
2019-04-04 22:06:02 +0100

    send mail to users a day before their flight schedule

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -1,10 +1,19 @@
 import os
+from datetime import timedelta
 
 from celery import shared_task
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import get_connection, EmailMultiAlternatives
+from django.db.models import Q
 from django.template.loader import render_to_string
+from django.utils import timezone
 from django.utils.html import strip_tags
+
+from api.helpers.utils import StatusChoices
+from .models import Booking
+from users.models import User
 
 @shared_task
 def email_ticket(ticket):
@@ -13,11 +22,11 @@ def email_ticket(ticket):
     from_email = settings.EMAIL_HOST_USER
     to_email = ticket['passenger']['email']
 
-    content_html = render_to_string('bookings/ticket.html', { 'ticket': ticket })
-    content_text = strip_tags(content_html)
+    html_content = render_to_string('bookings/ticket.html', { 'ticket': ticket })
+    text_content = strip_tags(html_content)
 
-    message = EmailMultiAlternatives(subject, content_text, from_email, [to_email])
-    message.attach_alternative(content_html, 'text/html')
+    message = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+    message.attach_alternative(html_content, 'text/html')
     message.send()
 
 @shared_task
@@ -27,9 +36,41 @@ def email_reservation(ticket):
     from_email = settings.EMAIL_HOST_USER
     to_email = ticket['passenger']['email']
 
-    content_html = render_to_string('bookings/confirmation.html', { 'ticket': ticket })
-    content_text = strip_tags(content_html)
+    html_content = render_to_string('bookings/confirmation.html', { 'ticket': ticket })
+    text_content = strip_tags(html_content)
 
-    message = EmailMultiAlternatives(subject, content_text, from_email, [to_email])
-    message.attach_alternative(content_html, 'text/html')
+    message = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+    message.attach_alternative(html_content, 'text/html')
     message.send()
+
+@periodic_task(
+    name='email_travel_reminder',
+    run_every=crontab(hour=8),
+    ignore_result=True
+)
+def email_travel_reminder():
+    connection = get_connection()
+    min_threshold = timezone.now() + timedelta(days=1)
+    max_threshold = timezone.now() + timedelta(days=2)
+
+    bookings = Booking.objects.filter(Q(flight_id__departure_datetime__gt=min_threshold) &
+                                      Q(flight_id__departure_datetime__lt=max_threshold),
+                                      flight_status=StatusChoices.R.name)
+    for booking in bookings:
+        subject = f'Reminder - Flight schedule to {booking.flight_id.destination}'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = booking.passenger_id.email
+
+        html_content = render_to_string('bookings/reminder.html', {
+            'ticket_number': booking.ticket_number,
+            'departure_datetime': booking.flight_id.departure_datetime,
+            'departure_airport': booking.flight_id.departing,
+            'arrival_datetime': booking.flight_id.arrival_datetime,
+            'destination_airport': booking.flight_id.destination,
+            'passenger': f'{booking.passenger_id.first_name} {booking.passenger_id.last_name}'
+        })
+        text_content = strip_tags(html_content)
+
+        message = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+        message.attach_alternative(html_content, 'text/html')
+        message.send()
